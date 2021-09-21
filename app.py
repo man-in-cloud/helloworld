810921b922581454fe12d13d7d4e95229a588cf9
man-in-cloud <91156347+man-in-cloud@users.noreply.github.com>
2021-09-21 10:21:38 -1000

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ t = input("Enter L for lbs or K for kilogram: ")
 t = str(t.lower())
 
 if t == "l":
-    a = w * 0.45
+    a = w / 0.45
     b = "Your weight in lbs is " + str(a)
 elif t == "k":
-    a = w / 0.45
+    a = w * 0.45
     b = "Your weight in kg is " + str(a)
 else:
     b = "Please enter L for lbs or K for kilogram"
